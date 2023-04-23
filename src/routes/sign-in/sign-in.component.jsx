004540c0acdab37ce034de6c307a57bc57fa7f78
James Aina <jydobee@Jamess-MacBook-Pro.local>
2023-04-23 14:35:50 -0600

    Setup initial routing

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import "../navigation/navigation.styles.scss"
+
+const SignIn = () => {
+    return (
+        <div>
+            <h1>I am the sign in page.</h1>
+        </div>
+    );
+}
+
+export default SignIn;
